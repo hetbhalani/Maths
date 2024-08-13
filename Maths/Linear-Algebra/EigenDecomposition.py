6e2a827768dcfd633a311a6236709c30e70be3c2
hetbhalani <bhalanihet2006@gmail.com>
2024-08-13 22:02:43 +0530

    EigenDecomposition

---
+++ Maths/Linear-Algebra/EigenDecomposition.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+v = np.array([3, 1])
+
+def plot_vectors(vectors, colors):
+    plt.figure()
+    plt.axvline(x=0, color='lightgray')
+    plt.axhline(y=0, color='lightgray')
+
+    for i in range(len(vectors)):
+        x = np.concatenate([[0, 0], vectors[i]])
+        plt.quiver([x[0]], [x[1]], [x[2]], [x[3]],
+                   angles='xy', scale_units='xy', scale=1, color=colors[i])
+    plt.xlim(-1, 5)
+    plt.ylim(-1, 2)
+    plt.show()
+
+plot_vectors([v], ['lightblue'])
