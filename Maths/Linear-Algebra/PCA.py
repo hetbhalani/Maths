b13017fe96035bc8c184a716d8f8cdda1f79293e
hetbhalani <bhalanihet2006@gmail.com>
2024-08-18 22:20:44 +0530

    PCA

---
+++ Maths/Linear-Algebra/PCA.py
@@ -0,0 +1,22 @@
+from sklearn import datasets
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+
+iris = datasets.load_iris()
+
+iris.data.shape
+iris.get("feature_names")
+iris.data[0:6,:]
+
+pca = PCA(n_components=2)
+X = pca.fit_transform(iris.data)
+X.shape
+
+print(X[0:6,:])
+plt.scatter(X[:, 0], X[:, 1])
+
+unique_elements, counts_elements = np.unique(iris.target, return_counts=True)
+np.asarray((unique_elements, counts_elements))
+
+plt.show()
