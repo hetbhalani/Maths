53f479b48f1b49c973dadad80e2d50cda740501d
hetbhalani <bhalanihet2006@gmail.com>
2024-08-18 18:06:25 +0530

    SVD

--- Maths/Linear-Algebra/SVD.py
+++ Maths/Linear-Algebra/SVD.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import matplotlib.pyplot as plt
+import numpy as np
 
 img = Image.open('oboe-with-book.jpg')
 plt.imshow(img)
@@ -8,5 +9,16 @@ imggray = img.convert('LA')
 # 'L' etle Luminance -> tivrata of light 
 # 'A' elte Alpha -> transparancy of light
 plt.imshow(imggray)
+
+imgmat = np.array(list(imggray.getdata(band=0)), dtype=float)
+imgmat.shape = (imggray.size[1], imggray.size[0])
+imgmat = np.matrix(imgmat)
+
+plt.imshow(imgmat, cmap='gray')
+
+U ,sigma, V = np.linalg.svd(imgmat)
+
+reconstrcted = np.matrix(U[:, :1]) * np.diag(sigma[:1]) * np.matrix(V[:1, :])
+plt.imshow(reconstrcted, cmap="gray")
 
 plt.show()
