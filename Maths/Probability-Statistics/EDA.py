90cb9bd342269af7c9aeef78a85b61001453ff0f
hetbhalani <bhalanihet2006@gmail.com>
2024-08-21 15:55:55 +0530

    plot EDA

--- Maths/Probability-Statistics/EDA.py
+++ Maths/Probability-Statistics/EDA.py
@@ -8,5 +8,10 @@ import numpy as np
 iris = pd.read_csv("iris.csv")
 iris.head()
 
-print(iris.shape)
-print(iris.columns)
+# print(iris.shape)
+# print(iris.columns)
+
+# print(iris["variety"].value_counts())
+
+iris.plot(kind='scatter', x='sepal.length', y='sepal.width')
+plt.show()
