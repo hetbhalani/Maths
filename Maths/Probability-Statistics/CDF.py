8511328e512a9b042cea4e48dabb021ea7d359de
hetbhalani <bhalanihet2006@gmail.com>
2024-08-22 10:20:21 +0530

    CDF plot

--- Maths/Probability-Statistics/CDF.py
+++ Maths/Probability-Statistics/CDF.py
@@ -12,16 +12,27 @@ iris_virginica = iris.loc[iris['variety'] == "Virginica"]
 iris_versicolor = iris.loc[iris['variety'] == "Versicolor"]
 
 # bins -> means how many data u want in graph
-count , edges = np.histogram(iris_setosa['petal.length'], bins=10, density=True)
-pdf = count/(sum(count))
-print(pdf,edges)
+count1, edges1 = np.histogram(iris_setosa['petal.length'], bins=10, density=True)
+pdf1 = count1 / sum(count1)
+cdf1 = np.cumsum(pdf1)
 
+count2, edges2 = np.histogram(iris_virginica['petal.length'], bins=10, density=True)
+pdf2 = count2 / sum(count2)
+cdf2 = np.cumsum(pdf2)
+
+count3, edges3 = np.histogram(iris_versicolor['petal.length'], bins=10, density=True)
+pdf3 = count3 / sum(count3)
+cdf3 = np.cumsum(pdf3)
+
+# for PDF
+plt.plot(edges1[1:], pdf1, label="Setosa PDF")
+plt.plot(edges2[1:], pdf2, label="Virginica PDF")
+plt.plot(edges3[1:], pdf3, label="Versicolor PDF")
 
 #for CDF
-
-cdf = np.cumsum(pdf) #cumsum etle Cumulative sum
-plt.plot(edges[1:],pdf)
-plt.plot(edges[1:],cdf)
+plt.plot(edges1[1:], cdf1, label="Setosa CDF", linestyle='--')
+plt.plot(edges2[1:], cdf2, label="Virginica CDF", linestyle='--')
+plt.plot(edges3[1:], cdf3, label="Versicolor CDF", linestyle='--')
 plt.xlabel('Petal Length')
 plt.ylabel('Probability')
 plt.grid(True)
