1946b4fd168bd30ee6632ac28d763f6471083d38
hetbhalani <bhalanihet2006@gmail.com>
2024-08-28 20:19:15 +0530

    mean and std-dev

--- Maths/Probability-Statistics/Mean-StanderdDeviation.py
+++ Maths/Probability-Statistics/Mean-StanderdDeviation.py
@@ -9,13 +9,14 @@ iris_setosa = iris.loc[iris['variety'] == "Setosa"]
 iris_virginica = iris.loc[iris['variety'] == "Virginica"]
 iris_versicolor = iris.loc[iris['variety'] == "Versicolor"]
 
-# use "mean" for Mean and use "std" for standers deviation in NumPy
+# use "mean" for Mean in NumPy
 print("Means = ")
 print(np.mean(iris_setosa['petal.length']))
 print(np.mean(np.append(iris_setosa['petal.length'],50)))
 print(np.mean(iris_virginica['petal.length']))
 print(np.mean(iris_versicolor['petal.length']))
 
+#  use "std" for standers deviation in NumPy
 print("Std-Dev = ")
 print(np.std(iris_setosa['petal.length']))
 print(np.std(iris_virginica['petal.length']))
