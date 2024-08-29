a86798f67b97c3d7e3133b0dc84ace17db981c7f
hetbhalani <bhalanihet2006@gmail.com>
2024-08-29 17:56:01 +0530

    box-cox

--- Maths/Probability-Statistics/BOX-COX.py
+++ Maths/Probability-Statistics/BOX-COX.py
@@ -6,5 +6,11 @@ ax1 = fig.add_subplot(211)
 x = stats.loggamma.rvs(5, size=500) + 5
 prob = stats.probplot(x,dist=stats.norm, plot=ax1)
 ax1.set_xlabel('')
-ax1.set_title('BOX-COX')
+ax1.set_title('before box cox')
+
+ax2 = fig.add_subplot(212)
+xt, _ = stats.boxcox(x)
+prob = stats.probplot(xt, dist=stats.norm, plot=ax2)
+ax2.set_title('after box cox')
+
 plt.show()
