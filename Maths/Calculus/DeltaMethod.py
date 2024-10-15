55ce694464c1dd878121dc86c0a0f394c532378d
hetbhalani <bhalanihet2006@gmail.com>
2024-10-15 10:34:32 +0530

    plot points

--- Maths/Calculus/DeltaMethod.py
+++ Maths/Calculus/DeltaMethod.py
@@ -10,11 +10,17 @@ def f(my_x):
 
 y = f(x)
 
+print(f(2))
+# answer is 10 so we have to find slope at 2,10 position
+print(f(5))
+# ans is 37 
+
 fig, ax = plt.subplots()
 plt.axvline(x=0, color='lightgray')
 plt.axhline(y=0, color='lightgray')
-plt.scatter(2,10) #ek point lidho k jena pr aapde slope joto 6
+plt.scatter(2,10) #point k jena pr aapde slope joto 6
+plt.scatter(5,37, c='orange', zorder=3) # ek bijo point lidho k jena relative aapde slope joye 6
+# zorder etle curve ni uper point aave
 ax.plot(x,y)
 
-print(f(2))
 plt.show()
